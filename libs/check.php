a332dbd924da56eb3f929207299443cd488a73c5
ahmedqo <ahmedqo1995@gmail.com>
2020-07-04 15:15:32 +0100

    Delete check.php

--- libs/check.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$data = array();
-if (isset($_SESSION['user']['id'])) {
-    $data['loged_in']  = true;
-} else {
-    $data['loged_in']  = false;
-}
-echo json_encode($data);
