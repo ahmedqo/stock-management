c1c36c21797187b4a7ba07eaccbbf4ce605997c6
ahmedqo <ahmedqo1995@gmail.com>
2020-07-04 15:25:25 +0100

    Add files via upload

---
+++ libs/report/lowStocks.php
@@ -0,0 +1,17 @@
+<?php
+ob_start();
+include_once("../classes.php");
+session_start();
+$data = array();
+$connection = new connection();
+$db = $connection->getconnection();
+$query = new report($db);
+$results = $query->lowStocks();
+if ($results) {
+    $data['type'] = "Success";
+    $data['message'] = $results;
+} else {
+    $data['type'] = "Error";
+    $data['message'] = "Something Goes Wrong";
+}
+echo json_encode($data);
